f43d4cc8760185c7ab322e17e91462cb4147d7d6
EDUMVA4 <edumva4@adsl-172-14-0-146.dsl.lgtpmi.sbcglobal.net>
2018-08-09 17:11:10 +0530

    Initial Commit

---
+++ IOSServer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSServer
+//
+//  Created by EDUMVA4 on 09/08/18.
+//  Copyright © 2018 EDUMVA4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
