e725c767c4d522bf2537526613f1d30a50c76ff7
kartikey pathak <kartikeypathak007>
2018-08-10 14:12:43 +0530

    hiii

--- IOSServer/ViewController.swift
+++ IOSServer/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello K.D.B.")
-        print("Hii")
+        print("Hello")
     }
 
     override func didReceiveMemoryWarning() {
